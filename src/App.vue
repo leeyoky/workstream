5806e34df3082d57dd4b24113243cc4558fb89a1
yeongkyung.lee <yeongkyung.lee@datastreams.co.kr>
2023-04-24 15:16:46 +0900

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,25 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="wrapper">
+    <login-wall></login-wall>
+    <login-page></login-page>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import LoginPage from './components/common/LoginPage.vue'
+import LoginWall from './components/common/LoginWall.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  components : {
+    LoginPage,
+    LoginWall
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+body{
+        margin: 0;
+        padding: 0;
+    }
 </style>
---
+++ src/components/common/LoginWall.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="login-main-wrapper">
+    </div>
+</template>
+
+<script>
+import $ from 'jquery';
+    
+export default {
+    mounted(){
+        $(".login-main-wrapper").ripples({
+            resolution: 346, // 렌더링 값이 클수록 잔물결 효과가 느리게 전파
+            perturbance: 0.02, // 잔물경 굴절 강도. 0은 굴절 없음
+        }); 
+    }
+}
+</script>
+
+<style>
+    .login-main-wrapper {
+         width: 100%;
+         height: 100vh;
+         background: url(main.jpg) no-repeat center; /* css 이미지 url */
+         background-size: cover;
+      }
+    .login-main-wrapper{
+        display: inline-block;
+    }
+
+    .login-main-wrapper img {
+        height: 100%;
+        width: 100%;
+    }
+</style>
